2c7e4e788af3d387a3057ac9d5f64ee9ed43692c
lin-bobo <1925036227@qq.com>
2018-07-25 17:41:19 +0800

    Creates splash activity and makes it the launched activity

---
+++ app/src/main/java/com/apress/gerber/currencies/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.apress.gerber.currencies;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash);
+    }
+}
