761910808f16b3b05360b9be908da0519d607fc4
lin-bobo <1925036227@qq.com>
2018-07-25 20:41:40 +0800

    Gets references to views defined in layout

--- app/src/main/java/com/apress/gerber/currencies/MainActivity.java
+++ app/src/main/java/com/apress/gerber/currencies/MainActivity.java
@@ -2,12 +2,30 @@ package com.apress.gerber.currencies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //define members that correspond to Views in our layout
+    private Button mCalcButton;
+    private TextView mConvertedTextView;
+    private EditText mAmountEditText;
+    private Spinner mForSpinner, mHomSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //assign references to our Views
+        mConvertedTextView = findViewById(R.id.txt_converted);
+        mAmountEditText = findViewById(R.id.edt_amount);
+        mCalcButton = findViewById(R.id.btn_calc);
+        mForSpinner = findViewById(R.id.spn_for);
+        mHomSpinner = findViewById(R.id.spn_hom);
+
     }
 }
