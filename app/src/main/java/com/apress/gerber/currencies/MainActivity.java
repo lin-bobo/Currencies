7021d9b8f4776ac538b76dd0652b85e6f90a6ce8
lin-bobo <1925036227@qq.com>
2018-07-25 21:14:25 +0800

    creates options menu

--- app/src/main/java/com/apress/gerber/currencies/MainActivity.java
+++ app/src/main/java/com/apress/gerber/currencies/MainActivity.java
@@ -2,6 +2,8 @@ package com.apress.gerber.currencies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -37,5 +39,30 @@ public class MainActivity extends AppCompatActivity {
         mForSpinner = findViewById(R.id.spn_for);
         mHomSpinner = findViewById(R.id.spn_hom);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+
+            case R.id.mnu_invert:
+                //TODO define behavior here
+                break;
+            case R.id.mnu_codes:
+                //TODO define behavior here
+                break;
+            case R.id.mnu_exit:
+                finish();
+                break;
+        }
+        return true;
     }
 }
