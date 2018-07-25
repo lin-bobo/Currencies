978e69c66fa8ac56580fbcef7304a66badb8ab55
lin-bobo <1925036227@qq.com>
2018-07-26 00:53:35 +0800

    Creates anon inner class to handle button behavior

--- app/src/main/java/com/apress/gerber/currencies/MainActivity.java
+++ app/src/main/java/com/apress/gerber/currencies/MainActivity.java
@@ -87,6 +87,13 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         }
 
+        mCalcButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //define behavior here
+            }
+        });
+
     }
 
     public boolean isOnline(){
