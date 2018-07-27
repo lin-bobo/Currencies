7de2732fcaaced283e5d2d79ff000a78233b14c0
lin-bobo <1925036227@qq.com>
2018-07-27 14:36:09 +0800

    Create proxy methods

--- app/src/main/java/com/apress/gerber/currencies/MainActivityTest.java
+++ app/src/main/java/com/apress/gerber/currencies/MainActivityTest.java
@@ -8,6 +8,8 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
@@ -46,5 +48,47 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
     @Override
     public void tearDown() throws Exception {
         super.tearDown();
+    }
+
+    public void proxyCurrencyConverterTask(final String str) throws Throwable{
+
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        mActivity.setCurrencyTaskCallback(new MainActivity.CurrencyTaskCallback() {
+            @Override
+            public void executionDone() {
+                latch.countDown();
+                assertEquals(convertToDouble(mConvertedTextView.getText().toString().substring(0, 5)), convertToDouble(str));
+            }
+        });
+        runTestOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mAmountEditText.setText(str);
+                mForSpinner.setSelection(0);
+                mHomSpinner.setSelection(0);
+                mCalcButton.performClick();
+            }
+        });
+
+        latch.await(30, TimeUnit.SECONDS);
+    }
+
+    public void testInteger() throws Throwable {
+        proxyCurrencyConverterTask("12");
+    }
+
+    public void testFloat() throws Throwable {
+        proxyCurrencyConverterTask("12..3");
+    }
+
+    private double convertToDouble(String str) throws NumberFormatException{
+        double dReturn = 0;
+        try {
+            dReturn = Double.parseDouble(str);
+        }catch (NumberFormatException e){
+            throw e;
+        }
+        return dReturn;
     }
 }
